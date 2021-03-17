8a319a19b67778d5fdd26976a86254d4476bea3f
Mauricio Tec <mauriciogtec@gmail.com>
2021-03-16 19:40:37 -0500

    updated density evaluation script

--- 6_densities_from_fitted_betas_opt_pt.jl
+++ 6_densities_from_fitted_betas_opt_pt.jl
@@ -5,6 +5,11 @@ using DelimitedFiles
 using CSV
 using DataFrames
 using Dates
+using DataStructures
+using Printf
+using ProgressMeter
+using Statistics
+using Base.Threads
 
 ##
 
@@ -16,8 +21,7 @@ splits = readtable("processed_data/splits_opt_pt.csv")
 
 ##
 vertexinfo = readtable("processed_data/vertex_data.csv")
-
-##
+num_nodes = size(vertexinfo, 1)
 
 
 # output settings
@@ -27,19 +31,7 @@ vertexinfo = readtable("processed_data/vertex_data.csv")
 num_files = 20
 smoothprobs_relpath() = "output_smooth_probs/"
 
-
-# read the data from the graph
-# actualy we only need the number of nodes
-types = Dict(
-    "nodes" => String,
-    "vertex" => Int,
-    "taz" => Int,
-    "hours" => Int,
-    "timelabel" => Date
-)
-graphdata = CSV.read(vertexinfo_relpath(), types=types)
-num_nodes = size(graphdata, 1)
-               
+##
 
 function getsplit(s::String)
     x = split(s, '_')[3]
@@ -47,51 +39,60 @@ function getsplit(s::String)
     return x
 end
 
+##
+
 # read files with betas
-betas = Dict{Tuple{Int, Int}, Vector{Float64}}()
-for file in betafiles
-    bin = getsplit(file)
-    a, b = parse.(Int, String.(split(bin, '-')))
-    β = vec(readdlm(joinpath(betas_relpath(), file)))
-    betas[a, b] = β
+num_splits = size(splits, 1)
+betas = zeros(num_nodes, num_splits)
+for (j, file) in enumerate(betafiles)
+    j = parse(Int, file[7:8]) + 1
+    betas[:, j] = vec(readdlm("best_betas/$file"))
 end
 
-splitlevels = vec(readdlm(splits_relpath()))
-nbins = length(splitlevels) - 1
+##
 
-# descend in each bin and multiplyprobabilities
-probs = ones(nbins, num_nodes)
-for (key, value) in betas
-    a = key[1] + 1
-    b = key[2]
-    m = (a + b) ÷ 2
-    p = 1.0 ./ (1.0 .+ exp.(-value))
-    p = reshape(p, 1, :)
-    probs[a:m, :] .*= p
-    probs[m:b, :] .*= 1.0 .- p
-end
-probs ./= sum(probs, dims=1)
+mid = Float64.(splits.mid)
+lows = Float64.(splits.lows)
+ups = Float64.(splits.ups)
+root = make_tree_from_bfs(lows, mid, ups)
 
-# write output
-if num_files == 1
-    fname = joinpath(smoothprobs_relpath(), "probs.csv")
-    writedlm(fname, probs, ',')
-else
-    chunksize = if num_nodes % num_files == 0
-        num_nodes ÷ num_files
-    else
-        (num_nodes ÷ num_files) + 1
+
+# nodes of interest
+
+
+
+#
+
+
+N = 100
+xseq = collect(range(0.0, 125.0, length=N))
+support = (lows[1], ups[1])
+log_probs = fill(-Inf, (num_nodes, N))
+pbar = Progress(num_nodes)
+
+@threads for i in 1:num_nodes
+    root = make_tree_from_bfs(lows, mid, ups, betas[i, :])
+    for j in 1:N
+        if support[1] ≤ xseq[j] ≤ support[2]
+            log_probs[i, j] = eval_logprob(root, xseq[j])
+        end
     end
-    for i in 1:num_files
-        from = (i - 1) * chunksize + 1
-        to = min(i * chunksize, num_nodes)
-        chunk = probs[:, from:to]
-        fname = joinpath(smoothprobs_relpath(), "probs-chunk_$i.csv")
-        writedlm(fname, chunk, ',')
-    end
+    next!(pbar)
 end
 
 
-# probs to densities
-# Δ = splitlevels[2:end] .- splitlevels[1:end-1]
-# dens = probs ./ Δ
+##
+
+pmat = exp.(log_probs)
+pmat ./= sum(pmat, dims=2)
+
+##
+
+pmatt = pmat'[:, range(1, num_nodes, step=100)]
+plot(pmatt, c=:gray, alpha=0.05)
+
+##
+
+##
+means = vec(sum(pmat .* xseq', dims=2))
+histogram(means)
